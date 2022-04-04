45c2daf95bb833f18323198cdf834f51719c11de
Minghong Xu <86758413+MinghongAlexXu@users.noreply.github.com>
2022-04-04 13:18:29 +0100

    Add numeric literals

--- src/#371_sum_of_two_integers/.py
+++ src/#371_sum_of_two_integers/.py
@@ -1,11 +1,11 @@
 class Solution:
     @staticmethod
     def getSum(a: int, b: int) -> int:
-        mask = 0xffffffff
+        mask = 0xff_ff_ff_ff
         a, b = a & mask, b & mask
         while b != 0:
             a, b = (a ^ b), (a & b) << 1 & mask
-        return a if a <= 0x7fffffff else ~(a ^ mask)
+        return a if a <= 0x7f_ff_ff_ff else ~(a ^ mask)
 
 assert Solution.getSum(1, 2) == 3
 assert Solution.getSum(2, 3) == 5
